6aff945cbef60230aea61e7c51f92390b59b02ca
b18007615 <ibrahim.boulahrouz@etu.univ-amu.fr>
2020-03-03 14:57:48 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+        <div>
+            SALUT LES POTES
+        </div>
+    <a href="SignIN.php"><button type="button"> Connexion </button></a>
+    </form>
+
+</head>
+<body>
+
+</body>
+</html>
