60f23ef6925158907e29da46f91a016499326ae3
Boulahrouz Ibrahim <ibrahim.boulahrouz@etu.univ-amu.fr>
2020-03-24 23:10:40 +0100

    sal

--- JS_css/decoreco.js
+++ JS_css/decoreco.js
@@ -5,7 +5,23 @@
 			
 			
 		})
-
+		$('.form').submit(function () {
+            $.ajax({
+                url : $(this).attr('action'),
+                method : $(this).attr('method'),
+                data : $(this).serialize()
+            }).done(function (data) {
+                if(data.success === true) {
+                   // window.location.href = 'index.php';
+				   console.log(data);
+                } else {
+                    $('.message').text(data.message);
+                }
+            }).fail(function () {
+                console.error('Une erreur critique est arrivée.');
+            });
+            return false;
+        });
 
 	})
 })();
