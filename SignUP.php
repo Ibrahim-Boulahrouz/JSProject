eb4be5a720d4797a675fe8bc7b8e98f9c8ad0d1b
Boulahrouz Ibrahim <ibrahim.boulahrouz@etu.univ-amu.fr>
2020-03-24 22:29:22 +0100

    c'est débile

--- SignUP.php
+++ SignUP.php
@@ -7,7 +7,7 @@ $obj->message = "Validé !";
 
 try {
     $dsn = 'mysql:host=mysql-ibrahim-boulahrouz.alwaysdata.net ;dbname=ibrahim-boulahrouz_db';
-    $bdd = new PDO($dsn, '189617_salut', 'IBrahim123-=');
+    $bdd = new PDO($dsn, '189617_salut', 'maximelepb');
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage()); // pas sécurisé
 }
